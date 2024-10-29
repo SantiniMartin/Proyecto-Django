783b83464037ca833cab17b467879acc0339de06
SantiniMartin <91801023+SantiniMartin@users.noreply.github.com>
2024-10-29 08:03:00 -0300

    Delete vacunacion directory

--- vacunacion/vacunacion/urls.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('', views.pagina_inicio, name="inicio"),
-    path('login/', views.login, name="login"),
-]
--- vacunacion/vacunacion/views.py
+++
@@ -1,8 +0,0 @@
-from django.shortcuts import render
-
-def pagina_inicio(request):
-    return render(request, 'pagina_de_inicio.html')
-
-def login(request):
-    print("METHOD:", request.method)
-    return render(request, 'login.html')
