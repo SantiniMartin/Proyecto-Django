265be8def8c8da4931155667e65037110162c1b4
Martin Santini <Martin_Santini219@outlook.es>
2024-11-11 10:17:01 -0300

    Se crea la app pacientes

---
+++ src/apps/pacientes/models.py
@@ -0,0 +1,19 @@
+from django.db import models
+
+GENERO_MASCULINO = 'M'
+GENERO_FEMENINO = 'F'
+GENERO_NO_BINARIO = 'X'
+
+GENERO_CHOICES = (
+    (GENERO_MASCULINO, 'Masculino'), 
+    (GENERO_FEMENINO, 'Femenino'), 
+    (GENERO_NO_BINARIO, 'No binario')
+)
+
+# Create your models here.
+class Paciente(models.Model):
+    nombre = models.CharField(max_length=50)
+    apellido = models.CharField(max_length=50)
+    dni = models.IntegerField()
+    fecha_nacimiento = models.DateField()
+    genero = models.CharField(choices=GENERO_CHOICES, max_length=10)
