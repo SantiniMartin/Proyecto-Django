e8668fced1a0cd3840e77e2f53fa5644ffb0b508
Martin Santini <Martin_Santini219@outlook.es>
2024-11-12 09:20:02 -0300

    Se añade vistas basadas en clases y paginación de listado de pacientes

--- src/apps/pacientes/urls.py
+++ src/apps/pacientes/urls.py
@@ -5,6 +5,9 @@ from .import views
 app_name = "pacientes"
 
 urlpatterns = [
-    path('lista/', views.lista, name='lista'),
-    path('nuevo/', views.nuevo, name='nuevo')
+    # path('lista/', views.lista, name='lista'),
+    path('lista/', views.Lista.as_view(), name='lista'),
+    # path('nuevo/', views.nuevo, name='nuevo')
+    path('nuevo/', views.Nuevo.as_view(), name='nuevo')
+    
 ]
--- src/apps/pacientes/views.py
+++ src/apps/pacientes/views.py
@@ -1,13 +1,42 @@
+from django.db.models.query import QuerySet
 from django.shortcuts import render, redirect
+from django.views.generic.edit import CreateView
+from django.views.generic.list import ListView
+from django.urls import reverse_lazy
+
 from .forms import FormPaciente
+from .models import Paciente
 
 # Create your views here.
-def lista(request):
+# def lista(request):
     
-    template_name = 'pacientes/lista.html'
+#     template_name = 'pacientes/lista.html'
     
-    return render(request, template_name)
+#     return render(request, template_name)
 
+class Lista(ListView):
+    template_name = 'pacientes/lista.html'
+    model = Paciente
+    context_object_name = 'pacientes'
+    paginate_by = 2
+    
+    def get_queryset(self):
+        return self.model.objects.all().order_by('nombre') #.filter(nombre='Antonela')
+
+
+class Nuevo(CreateView):
+    template_name = 'pacientes/nuevo.html'
+    model = Paciente
+    form_class = FormPaciente
+    success_url = reverse_lazy('pacientes:lista')
+    
+    def get_context_data(self, **kwargs):
+        ctx = super(Nuevo, self).get_context_data(**kwargs)
+        ctx['titulo'] = 'Nuevo Paciente'
+        print("Hola estoy en el get context")
+        return ctx
+    
+"""
 def nuevo(request):
     
     template_name = 'pacientes/nuevo.html'
@@ -29,3 +58,4 @@ def nuevo(request):
     }
     
     return render(request, template_name, ctx)
+"""
